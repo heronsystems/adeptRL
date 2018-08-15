213a694d773fb385e3d5ee70faacb73293917993
benbellheron <ben.bell@heronsystems.com>
2018-08-15 09:34:37 -0400

    Square p2p communication

--- adept/containers/mpi/p2pcommunication.py
+++ adept/containers/mpi/p2pcommunication.py
@@ -1,3 +1,6 @@
+import enum
+
+
 class P2PCommunicationProtocol:
     def __init__(self, comm):
         self.size = comm.Get_size()
@@ -15,17 +18,11 @@ class P2PCommunicationProtocol:
 class P2PRoundRobin(P2PCommunicationProtocol):
     @property
     def next_dest(self):
-        if self.rank == self.size - 1:
-            return 0
-        else:
-            return self.rank + 1
+        return (self.rank + 1) % self.size
 
     @property
     def next_source(self):
-        if self.rank == 0:
-            return self.size - 1
-        else:
-            return self.rank - 1
+        return (self.rank - 1) % self.size
 
 
 class P2PReversingRoundRobin(P2PCommunicationProtocol):
@@ -42,35 +39,55 @@ class P2PReversingRoundRobin(P2PCommunicationProtocol):
             self._current_step = 0
 
         if self._dir:
-            if self.rank == self.size - 1:
-                return 0
-            else:
-                return self.rank + 1
+            return (self.rank + 1) % self.size
         else:
-            if self.rank == 0:
-                return self.size - 1
-            else:
-                return self.rank - 1
-
+            return (self.rank - 1) % self.size
     @property
     def next_source(self):
         if self._dir:
-            if self.rank == 0:
-                return self.size - 1
-            else:
-                return self.rank - 1
+            return (self.rank - 1) % self.size
         else:
-            if self.rank == self.size - 1:
-                return 0
-            else:
-                return self.rank + 1
+            return (self.rank + 1) % self.size
+
+
+class SquareDirections(enum.IntEnum):
+    FORWARD = 0
+    ACROSS = 1
+    BACKWARD = 2
+
+class P2PReversingSquare(P2PCommunicationProtocol):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self._dir = -1
+
+    @property
+    def next_dest(self):
+        self._dir = self._next_dir()
+        if self._dir == SquareDirections.FORWARD:
+            return (self.rank + 1) % self.size
+        elif self._dir == SquareDirections.ACROSS:
+            return (self.rank + 2) % self.size
+        else:  # SquareDirections.BACKWARD
+            return (self.rank - 1) % self.size
+
+    @property
+    def next_source(self):
+        if self._dir == SquareDirections.FORWARD:
+            return (self.rank - 1) % self.size
+        elif self._dir == SquareDirections.ACROSS:
+            return (self.rank - 2) % self.size
+        else:  # Direction is backward so listen to the node in front of me
+            return (self.rank + 1) % self.size
+
+    def _next_dir(self):
+        return (self._dir + 1) % 3
 
 
 def P2PBestProtocol(comm):
     size = comm.Get_size()
     if size == 2:
         return P2PRoundRobin(comm)
-    if size >= 3:
+    if size == 3:
         return P2PReversingRoundRobin(comm)
-    # if size == 4:
-    #     return P2PReversingSquare(comm)
+    if size == 4:
+        return P2PReversingSquare(comm)
