77c10633a2ef821df4a685a0100a527b2715223e
jtatusko <jtatusko@gmail.com>
2018-12-26 16:50:06 -0500

    fix test

--- tests/utils/test_util.py
+++ tests/utils/test_util.py
@@ -5,8 +5,7 @@ from adept.utils.util import listd_to_dlist, dlist_to_listd
 
 class TestUtil(unittest.TestCase):
     def test_dlist_to_listd(self):
-        # assert dlist_to_listd({'a': [1]}) == [{'a': 1}]
-        assert None == 1
+        assert dlist_to_listd({'a': [1]}) == [{'a': 1}]
 
     def test_listd_to_dlist(self):
         assert listd_to_dlist([{'a': 1}]) == {'a': [1]}
