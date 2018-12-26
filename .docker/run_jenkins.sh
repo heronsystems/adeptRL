79e0b6335e2773eb9a04b019d1ca7f0c55ac4c9d
jtatusko <jtatusko@gmail.com>
2018-12-26 11:02:22 -0500

    jenkinsfile

--- .docker/run_jenkins.sh
+++ .docker/run_jenkins.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo docker run \
-    --rm \
-    -u root \
-    -it \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v "$HOME":/home \
-    -p 8080:8080 \
-    jenkinsci/blueocean
+docker run \
+  --rm \
+  -u root \
+  -p 8080:8080 \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$HOME":/home \
+  jenkinsci/blueocean
