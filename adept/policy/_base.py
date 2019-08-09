6f898c7bc17290070164fddf3a4739ba7f92a4ce
jtatusko <jtatusko@gmail.com>
2019-08-09 16:21:24 -0400

    cleaner policy

--- adept/policy/_base.py
+++ adept/policy/_base.py
@@ -35,21 +35,8 @@ class _BasePolicy(metaclass=abc.ABCMeta):
 
 class ActorCriticHelper(_BasePolicy, metaclass=abc.ABCMeta):
     """
-    A helper class for actor critic policies. Uses a cache to prevent
-    recomputation.
+    A helper class for actor critic policies.
     """
-
-    def __init__(self, action_space):
-        """
-        :param action_space: Space
-        """
-        super(ActorCriticHelper, self).__init__(action_space)
-
-        # Initialize cache
-        self._cache = {
-            'log_probs': None,
-            'probs': None
-        }
 
     def act(self, logits, available_actions=None):
         """
@@ -61,10 +48,7 @@ class ActorCriticHelper(_BasePolicy, metaclass=abc.ABCMeta):
         """
         raise NotImplementedError
 
-    def clear_cache(self):
-        self._cache = {k: None for k in self._cache.keys()}
-
-    def flatten_logit(self, logit):
+    def flatten_to_2d(self, logit):
         """
         :param logits: Tensor of arbitrary dim
         :return: logits flattened to (N, X)
@@ -86,52 +70,86 @@ class ActorCriticHelper(_BasePolicy, metaclass=abc.ABCMeta):
     def softmax(self, logit):
         """
         :param logit: torch.Tensor (N, X)
-        :return:
+        :return: torch.Tensor (N, X)
         """
-        if self._cache['probs']:
-            return self._cache['probs']
-
-        probs = F.softmax(logit, dim=1)
-        self._cache['probs'] = probs
-        return probs
+        return F.softmax(logit, dim=1)
 
     def log_softmax(self, logit):
-        if self._cache['log_probs']:
-            return self._cache['log_probs']
+        """
+        :param logit: torch.Tensor (N, X)
+        :return: torch.Tensor (N, X)
+        """
+        return F.log_softmax(logit, dim=1)
 
-        log_probs = F.log_softmax(logit, dim=1)
-        self._cache['log_probs'] = log_probs
-        return log_probs
+    def log_probability(self, log_softmax, action):
+        return log_softmax.gather(1, action.unsqueeze(1))
 
-    def log_probability(self, logit, action):
-        return self.log_softmax(logit).gather(1, action.unsqueeze(1))
-
-    def entropy(self, logit):
+    def entropy(self, log_softmax, softmax):
         return -(
-            self.log_softmax(logit) * self.softmax(logit)
+            log_softmax * softmax
         ).sum(1, keepdim=True)
 
-    def sample_action(self, logit):
-        return self.softmax(logit).multinomial(1).squeeze(1)
+    def sample_action(self, softmax):
+        """
+        Samples an action from a softmax distribution.
+
+        :param softmax: torch.Tensor (N, X)
+        :return: torch.Tensor (N)
+        """
+        return softmax.multinomial(1).squeeze(1)
+
+    def select_action(self, softmax):
+        """
+        Selects the action with the highest probability.
+
+        :param softmax:
+        :return:
+        """
+        return torch.argmax(softmax, dim=1)
 
 
 class ActorCriticPolicy(ActorCriticHelper):
     def act(self, logits, available_actions=None):
+        """
+        :param logits: Dict[ActionKey, torch.Tensor]
+        :param available_actions: torch.Tensor (N, NB_ACTION), one hot
+        :return:
+        """
         actions = OrderedDict()
         log_probs = []
         entropies = []
-        for key in self._action_keys:
-            logit = self.flatten_logit(logits[key])
-            action = self.sample_action(logit)
 
-            log_probs.append(self.log_probability(logit, action))
-            entropies.append(self.entropy(logit))
+        for key in self._action_keys:
+            logit = self.flatten_to_2d(logits[key])
+
+            log_softmax, softmax = self.log_softmax(logit), self.softmax(logit)
+            entropy = self.entropy(log_softmax, softmax)
+            entropies.append(entropy)
+            action = self.sample_action(softmax)
+            log_probs.append(self.log_probability(log_softmax, action))
             actions[key] = action.cpu()
-            self.clear_cache()
 
         log_probs = torch.cat(log_probs, dim=1)
         entropies = torch.cat(entropies, dim=1)
+
         return actions, {
             'log_probs': log_probs,
             'entropies': entropies
         }
+
+
+class ActorCriticEvalPolicy(ActorCriticHelper):
+    def act(self, logits, available_actions=None):
+        """
+        :param logits: Dict[ActionKey, torch.Tensor]
+        :param available_actions:
+            None if not needed
+            torch.Tensor (N, NB_ACTION), one hot
+        :return:
+        """
+        with torch.no_grad():
+            actions = OrderedDict()
+            for key in self._action_keys:
+                logit = self.flatten_to_2d(logits[key])
+                actions[key] = self.select_action(self.softmax(logit)).cpu()
+            return actions, {}
