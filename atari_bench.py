fa13b0f90a7dd4bbf9e373890606eeec4777a62e
jtatusko <jtatusko@gmail.com>
2019-08-21 12:02:24 -0400

    try calling thru app

--- atari_bench.py
+++ atari_bench.py
@@ -3,16 +3,14 @@ from subprocess import call
 import os
 
 for env in ATARI_ENVS:
-    exit(
-        call([
-            'python',
-            '-m',
-            'adept.scripts.distrib',
-            f'--env {env}',
-            '--nb-env 128',
-            '--nb-step 50e6',
-            '--eval',
-            '--use-defaults',
-            '--logdir ~/Documents/atari_bench'
-        ], env=os.environ)
-    )
+    call([
+        'python',
+        '-m',
+        'adept.app distrib',
+        f'--env {env}',
+        '--nb-env 128',
+        '--nb-step 50e6',
+        '--eval',
+        '-y',
+        '--logdir ~/Documents/atari_bench'
+    ], env=os.environ)
