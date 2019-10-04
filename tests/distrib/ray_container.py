7eb6809e343f9a13d57c44c38bb22e4758cf5bf9
jtatusko <jtatusko@gmail.com>
2019-10-04 08:25:49 -0400

    nccl setup in ray actors

--- tests/distrib/ray_container.py
+++ tests/distrib/ray_container.py
@@ -1,18 +1,20 @@
 import ray
+from torch import distributed as dist
+from itertools import cycle
 import torch
-# shared state
-# step count
-# queue
 
-# main script will instantiate as many hosts/workers as requested
-# all workers compute exp
-# host wait for n to be ready
-# when ready, merge batch and learn
+
 @ray.remote(num_gpus=0.25)
 class Host:
-    def __init__(self):
-        self.stuff = 'stuff'
-        print(ray.get_gpu_ids())
+    def __init__(self, rank, port, world_size):
+        dist.init_process_group(
+            'nccl',
+            init_method='tcp://127.0.0.1:{}'.format(port),
+            rank=rank,
+            world_size=world_size
+        )
+        self.learner_group = None
+        self.worker_group = None
 
     def hello(self):
         print('hello')
@@ -20,23 +22,50 @@ class Host:
 
 @ray.remote(num_gpus=0.25)
 class Worker:
-    def __init__(self):
-        print(ray.get_gpu_ids())
-        self.stuff = 'stuff'
+    def __init__(self, rank, port, world_size):
+        dist.init_process_group(
+            'nccl',
+            init_method='tcp://127.0.0.1:{}'.format(port),
+            rank=rank,
+            world_size=world_size
+        )
+        self.cur_exp = None
+        self.worker_group = None
 
     def hello(self, name):
         print('hello', name)
 
+    def compute_exp(self):
+        self.cur_exp = torch.ones()
+
+    def sync(self):
+        pass
+
 
 if __name__ == '__main__':
     nb_host = 2
+    nb_worker = 2
+    port = 6009
+    world_size = nb_host + nb_worker
     names = ['joe', 'ben']
 
     ray.init(num_gpus=1)
-    print(ray.get_gpu_ids())
-    remote_hosts = [Host.remote() for _ in range(nb_host)]
 
-    remote_workers = [Worker.remote() for _ in range(nb_host)]
+    # instantiate as many hosts/workers as requested
+    remote_hosts = [
+        Host.remote(rank, port, world_size) for rank in range(nb_host)
+    ]
+    host_gen = cycle(range(nb_host))
+
+    remote_workers = [
+        Worker.remote(rank + nb_host, port, world_size) for rank in range(nb_worker)
+    ]
+
+    # all workers compute exp
+    # host wait for n to be ready
+    # when ready, merge batch and learn
+    step_count = 0
+    queue = []
 
     for h in remote_hosts:
         ray.get(h.hello.remote())
