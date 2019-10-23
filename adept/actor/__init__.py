53d6a5cb9989b17d823639c11af8eed4a62d0d5f
Joe Tatusko <jtatusko@gmail.com>
2019-10-23 09:42:30 -0400

    sampling eval actor, tcp subproc env

--- adept/actor/__init__.py
+++ adept/actor/__init__.py
@@ -1,11 +1,12 @@
 from .base.actor_module import ActorModule
 from .ac_rollout import ACRolloutActorTrain
-from adept.actor.ac_eval import ACActorEval
+from adept.actor.ac_eval import ACActorEval, ACActorEvalSample
 from .impala import ImpalaHostActor, ImpalaWorkerActor
 
 ACTOR_REG = [
     ACRolloutActorTrain,
     ACActorEval,
+    ACActorEvalSample,
     ImpalaHostActor,
     ImpalaWorkerActor
 ]
