8adaacdcb3bf02db592995c3c664bbc49d34ffee
Joe Tatusko <jtatusko@gmail.com>
2020-09-28 09:28:51 -0400

    0.4.0 dev

--- adept/globals.py
+++ adept/globals.py
@@ -12,4 +12,4 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-VERSION = "0.3.0"
+VERSION = "0.4.0dev0"
