fd0331fe5db15b2445c23c992e668a0dead3788b
Joseph Banks <jsb6md@virginia.edu>
2020-09-29 10:25:41 -0400

    new preprocessing

--- adept/preprocess/observation.py
+++ adept/preprocess/observation.py
@@ -12,8 +12,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from copy import deepcopy
-
+from copy import copy, deepcopy
+from adept.preprocess.ops2 import MultiOperation, Operation
 
 class ObsPreprocessor:
     def __init__(self, ops, observation_space, observation_dtypes=None):
@@ -69,3 +69,75 @@ class ObsPreprocessor:
             del prev[name]
         update = fn(cur)
         return {**prev, **update}
+
+
+class Preprocessor:
+    def __init__(self, ops, observation_space, observation_dtypes=None):
+        """
+        :param ops: List[Operation]
+        :param observation_space: Dict[ObsKey, Shape]
+        :param observation_dtypes: Dict[ObsKey, dtype_str]
+        """
+        cur_space = deepcopy(observation_space)
+        cur_dtypes = deepcopy(observation_dtypes)
+
+        for op in ops:
+            names = list(cur_space.keys())
+
+            cur_space = self._update(names, cur_space, op.update_shape)
+            if observation_dtypes:
+                cur_dtypes = self._update(names, cur_dtypes, op.update_dtype)
+
+        self.ops = ops
+        self.observation_space = cur_space
+        self.observation_dtypes = cur_dtypes
+
+    def _bld_rank_to_names(self, obs_space):
+        d = {1: [], 2: [], 3: [], 4: []}
+        for name, shape in obs_space.items():
+            d[len(shape)].append(name)
+        return d
+
+    def _update(self, names, prev, fn):
+        cur = {}
+        for name in names:
+            cur[name] = prev[name]
+            del prev[name]
+        update = fn(cur)
+        return {**prev, **update}
+
+
+class CPUPreprocessor(Preprocessor):
+    def __call__(self, obs):
+        obs = copy(obs)
+        for op in self.ops:
+            if issubclass(type(op), Operation):
+                obs[op.output_field] = op.preprocess_gpu(obs[op.input_field])
+            elif issubclass(type(op), MultiOperation):
+                input_tensors = [obs[k] for k in op.input_fields]
+                output_tensors = op.preprocess_gpu(input_tensors)
+                for k, tensor in zip(op.output_fields, output_tensors):
+                    obs[k] = tensor
+        return obs
+
+    def reset(self):
+        for o in self.ops:
+            o.reset()
+
+
+class GPUPreprocessor(Preprocessor):
+    def __call__(self, obs):
+        obs = copy(obs)
+        for op in self.ops:
+            if issubclass(type(op), Operation):
+                obs[op.output_field] = op.preprocess_gpu(obs[op.input_field])
+            elif issubclass(type(op), MultiOperation):
+                input_tensors = [obs[k] for k in op.input_fields]
+                output_tensors = op.preprocess_gpu(input_tensors)
+                for k, tensor in zip(op.output_fields, output_tensors):
+                    obs[k] = tensor
+        return obs
+
+    def to(self, device):
+        self.ops = [op.to(device) for op in self.ops]
+        return self
