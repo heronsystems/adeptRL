acfd1517f9e014c89785a3d89b32bd55586f0e16
Joe Tatusko <jtatusko@gmail.com>
2020-12-29 16:24:35 -0500

    update agent stub

--- examples/custom_agent_stub.py
+++ examples/custom_agent_stub.py
@@ -1,62 +1,37 @@
 """
 Use a custom agent.
 """
-from adept.agents import AgentModule, AgentRegistry
+
+import typing
+from typing import Dict, Tuple
+
+from adept.agent import AgentModule
 from adept.scripts.local import parse_args, main
+
+if typing.TYPE_CHECKING:
+    import torch
+    from adept.network import NetworkModule
+    from adept.rewardnorm import RewardNormModule
 
 
 class MyCustomAgent(AgentModule):
     # You will be prompted for these when training script starts
     args = {"example_arg1": True, "example_arg2": 5}
 
-    def __init__(
-        self,
-        network,
-        device,
-        reward_normalizer,
-        gpu_preprocessor,
-        engine,
-        action_space,
-        nb_env,
-        *args,
-        **kwargs
-    ):
+    def __init__(self, reward_normalizer, action_space, spec_builder):
         super(MyCustomAgent, self).__init__(
-            network,
-            device,
-            reward_normalizer,
-            gpu_preprocessor,
-            engine,
-            action_space,
-            nb_env,
+            reward_normalizer, action_space,
         )
 
     @classmethod
     def from_args(
         cls,
         args,
-        network,
-        device,
-        reward_normalizer,
-        gpu_preprocessor,
-        engine,
-        action_space,
+        reward_normalizer: RewardNormModule,
+        action_space: Dict[str, Tuple[int, ...]],
+        spec_builder,
         **kwargs
     ):
-        """
-
-        ArgName = str
-
-        :param args: Dict[ArgName, Any]
-        :param network: BaseNetwork
-        :param device: torch.device
-        :param reward_normalizer: Callable[[float], float]
-        :param gpu_preprocessor: ObsPreprocessor
-        :param engine: env_registry.Engines
-        :param action_space: Dict[ActionKey, torch.Tensor]
-        :param kwargs:
-        :return: MyCustomAgent
-        """
         pass
 
     @property
@@ -69,62 +44,30 @@ class MyCustomAgent(AgentModule):
         pass
 
     @staticmethod
-    def output_space(action_space):
-        """
-        Merge action space with any agent-based outputs to get an output_space.
-
-        ActionKey = str
-        Shape = Tuple[*int]
-
-        :param action_space: Dict[ActionKey, Shape]
-        :return:
-        """
+    def output_space(action_space: Dict[str, Tuple[int, ...]]):
+        """Merge action space with any agent-based outputs to get an output_space."""
         pass
 
-    def compute_loss(self, experience, next_obs):
-        """
-        Compute losses.
-
-        ObsKey = str
-        LossKey = str
-
-        :param experience: Tuple[*Any]
-        :param next_obs: Dict[ObsKey, torch.Tensor]
-        :return: Dict[LossKey, torch.Tensor (0D)]
-        """
+    def compute_loss(
+        self, experience: Tuple[torch.Tensor], next_obs: Dict[str, torch.Tensor]
+    ):
+        """Compute losses."""
         pass
 
-    def act(self, obs):
-        """
-        Generate an action.
-
-        ObsKey = str
-        ActionKey = str
-
-        :param obs: Dict[ObsKey, torch.Tensor]
-        :return: Dict[ActionKey, np.ndarray]
-        """
-        pass
-
-    def act_eval(self, obs):
-        """
-        Generate an action in an evaluation.
-
-        ObsKey = str
-        ActionKey = str
-
-        :param obs: Dict[ObsKey, torch.Tensor]
-        :return: Dict[ActionKey, np.ndarray]
-        """
+    def act(
+        self,
+        network: NetworkModule,
+        obs: Dict[str, torch.Tensor],
+        prev_internals: Dict[str, torch.Tensor],
+    ):
+        """Generate an action."""
         pass
 
 
 if __name__ == "__main__":
     args = parse_args()
-    agent_reg = AgentRegistry()
-    agent_reg.register_agent(MyCustomAgent)
 
-    main(args, agent_registry=agent_reg)
+    main(args)
 
     # Call script like this to train agent:
     # python -m custom_agent_stub.py --agent MyCustomAgent
